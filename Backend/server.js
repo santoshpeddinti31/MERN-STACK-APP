d04a5a232c54b6cfd9058f63455265ef10f98da5
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-13 13:27:59 +0530

    both updated

--- Backend/server.js
+++ Backend/server.js
@@ -27,13 +27,6 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
 //connect to database
 connectToDb();
 
