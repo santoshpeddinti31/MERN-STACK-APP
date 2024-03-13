b985c92f9e235e0a4e611f46dac5b0bbd8b7308c
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-13 19:47:03 +0530

    all ok

--- Backend/server.js
+++ Backend/server.js
@@ -6,15 +6,13 @@ if (process.env.NODE_ENV != "production") {
 
 //important dependencies
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const connectToDb = require("./config/connectToDb.js");
 const todosController = require("./controllers/controller.js");
 const usersController = require("./controllers/userController.js");
 const cookieParser = require("cookie-parser");
 const requireAuth = require("./middleware/requireAuth.js");
-
-//create an express app
-const app = express();
 
 //configure express app
 app.use(express.json());
@@ -24,19 +22,10 @@ app.use(
   cors({
     origin: true,
     credentials: true,
-    optionsSuccessStatus: 200,
   })
 );
 
 app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
   res.send("nodejs");
 });
 
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://mern-stack-app-iota.vercel.app";
 axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
