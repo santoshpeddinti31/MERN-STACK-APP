4df0042bd422ba63416e36332e1fc2da901dc430
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-13 13:33:22 +0530

    updated config in backend

--- Backend/config/connectToDb.js
+++ Backend/config/connectToDb.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectToDb() {
   try {
+    mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.DB_URL);
     console.log("Database is connected");
   } catch (error) {
