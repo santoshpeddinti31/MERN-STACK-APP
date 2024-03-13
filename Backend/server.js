71728f2f674645da46a9e533fe2cf9681f162ce6
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-13 16:15:55 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -28,6 +28,17 @@ app.use(
   })
 );
 
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
+
 //connect to database
 connectToDb();
 
