21211ffed50635cdc18d54bbe63f18827f5678a9
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-13 13:05:33 +0530

    cors methods add

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: true,
+    credentials: true,
+    methods: ["GET", "PUT", "POST", "UPDATE"],
   })
 );
 
