5030de173577b3493ac7957fc552810e7a5f0979
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-14 12:10:36 +0530

    frontend updated

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
+// backend url : https://mern-stack-app-theta.vercel.app/
+
+axios.defaults.baseURL = "https://mern-stack-app-theta.vercel.app/";
 axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
